f2851774fc3c427740177e3b35dbd980f2f04683
Sean Harmeyer <sean.g.harmeyer@nasa.gov>
2025-07-11 10:14:19 -0500

    turn off old graphics
    Add missing requirements file because DAN doesn't know how to CODE BRO
    
    update .gitignore

--- trick_sims/SIM_satellite/RUN_test/input.py
+++ trick_sims/SIM_satellite/RUN_test/input.py
@@ -1,17 +1,17 @@
 import os
 
-exec(open("Modified_data/realtime.py").read())
-#exec(open("Modified_data/Satellite.dr").read())
+#exec(open("Modified_data/realtime.py").read())
+exec(open("Modified_data/Satellite.dr").read())
 
 dyn_integloop.getIntegrator(trick.Runge_Kutta_4, 18)
 
 scene_app_path = 'models/Satellite/graphics/Scene'
-if os.path.exists(scene_app_path):
-    dyn.sat_graph_comm.syscmd = scene_app_path + ' &'
-else:
-    dyn.sat_graph_comm.syscmd = '';
-    sys.stderr.write(scene_app_path + ' not found. It probably needs to be complied.\n')
-    sys.exit(1)
+#if os.path.exists(scene_app_path):
+#    dyn.sat_graph_comm.syscmd = scene_app_path + ' &'
+#else:
+#    dyn.sat_graph_comm.syscmd = '';
+#    sys.stderr.write(scene_app_path + ' not found. It probably needs to be complied.\n')
+#    sys.exit(1)
 
 # Fire Thrusters at the given times.
 read = 10
